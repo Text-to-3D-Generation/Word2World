5ef288a0b5131db14b65d6a631b1c0bdc187f8c1
Basel Hossam <102327565+BaselHossam97@users.noreply.github.com>
2025-05-26 04:21:53 +0300

    del old file

--- diff-gaussian-rasterization/cuda_rasterizer/gaussianTiles.h
+++
@@ -1,9 +0,0 @@
-#ifndef GAUSSIAN_TILES_H_INCLUDED
-#define GAUSSIAN_TILES_H_INCLUDED
-
-#include <cuda_runtime.h>
-#include <stdint.h>
-
-__global__ void gaussianTiles(int L, uint64_t* keysArray, uint2* tileSliceStartEnd);
-
-#endif // GAUSSIAN_TILES_H_INCLUDED
