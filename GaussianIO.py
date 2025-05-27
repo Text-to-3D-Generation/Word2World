27ed35ed0d1b6839ee535204b4bfa83d5f6ea9c1
Ahmed Hany <ahmed.abdelgawad011@eng-st.cu.edu.eg.com>
2025-05-27 11:01:55 +0300

    GaussianIO.py modification

--- GaussianIO.py
+++ GaussianIO.py
@@ -1,96 +1,72 @@
 import os
 import numpy as np
-from typing import NamedTuple
 from plyfile import PlyData, PlyElement
-import torch
-import torch.nn as nn
 from misc_utils import flatten_sh_coefficients
+
+
 class GaussianIO:
-    """Handles all input/output operations for GaussianModel"""
     
     @staticmethod
-    def save_ply(
-        path: str,
-        mean: torch.Tensor,
-        sh_coefficients_dc: torch.Tensor,
-        sh_coefficients_ac: torch.Tensor,
-        opacities: torch.Tensor,
-        svec: torch.Tensor,
-        quaternions: torch.Tensor,
-    ):
-        """Save Gaussian model to PLY file"""
+    def save_ply(path,mean,sh_coefficients_dc,sh_coefficients_ac,opacities,svec,quaternions):
         os.makedirs(os.path.dirname(path), exist_ok=True)
-        
-        # Convert tensors to numpy
-        mean_np = mean.detach().cpu().numpy()
-        normals = np.zeros_like(mean_np)
-        sh_coefficients_dc_np = flatten_sh_coefficients(sh_coefficients_dc)
-        sh_coefficients_ac_np = flatten_sh_coefficients(sh_coefficients_ac)
-        opacities_np = opacities.detach().cpu().numpy()
-        svec_np = svec.detach().cpu().numpy()
-        quaternions_np = quaternions.detach().cpu().numpy()
+        position_data = mean.detach().cpu().numpy()
+        dc_flat = flatten_sh_coefficients(sh_coefficients_dc)
+        ac_flat = flatten_sh_coefficients(sh_coefficients_ac)
+        opacity_data = opacities.detach().cpu().numpy()
+        scale_data = svec.detach().cpu().numpy()
+        rotation_data = quaternions.detach().cpu().numpy()
+        prop_list = [('x', 'f4'), ('y', 'f4'), ('z', 'f4')]
+        for i in range(dc_flat.shape[1]):
+            prop_list.append((f'sh_coefficients_dc_{i}', 'f4'))
+        for i in range(ac_flat.shape[1]):
+            prop_list.append((f'sh_coefficients_ac_{i}', 'f4'))
+        prop_list.append(('opacity', 'f4'))
+        for i in range(scale_data.shape[1]):
+            prop_list.append((f'svec_{i}', 'f4'))
+        for i in range(rotation_data.shape[1]):
+            prop_list.append((f'quaternion_{i}', 'f4'))
 
-        attrs = ['x', 'y', 'z', 'nx', 'ny', 'nz']
-        attrs += [f'sh_coefficients_dc_{i}' for i in range(sh_coefficients_dc.shape[1] * sh_coefficients_dc.shape[2])]
-        attrs += [f'sh_coefficients_ac_{i}' for i in range(sh_coefficients_ac.shape[1] * sh_coefficients_ac.shape[2])]
-        attrs += ['opacity']
-        attrs += [f'svec_{i}' for i in range(svec.shape[1])]
-        attrs += [f'quaternion_{i}' for i in range(quaternions.shape[1])]
-        
-        # Create structured array
-        dtype_full = [(attr, 'f4') for attr in attrs]
-        elements = np.empty(mean_np.shape[0], dtype=dtype_full)
-        data = np.concatenate((mean_np, normals, sh_coefficients_dc_np, sh_coefficients_ac_np, opacities_np, svec_np, quaternions_np), axis=1)
-        elements[:] = list(map(tuple, data))
-        
-        # Write to file
-        PlyData([PlyElement.describe(elements, 'vertex')]).write(path)
+        dtype = np.dtype(prop_list)
+        vertex_count = position_data.shape[0]
+        array_data = np.empty(vertex_count, dtype=dtype)
+        array_data['x'] = position_data[:, 0]
+        array_data['y'] = position_data[:, 1]
+        array_data['z'] = position_data[:, 2]
+        for i in range(dc_flat.shape[1]):
+            array_data[f'sh_coefficients_dc_{i}'] = dc_flat[:, i]
+        for i in range(ac_flat.shape[1]):
+            array_data[f'sh_coefficients_ac_{i}'] = ac_flat[:, i]
+        array_data['opacity'] = opacity_data[:, 0]
+        for i in range(scale_data.shape[1]):
+            array_data[f'svec_{i}'] = scale_data[:, i]
+        for i in range(rotation_data.shape[1]):
+            array_data[f'quaternion_{i}'] = rotation_data[:, i]
+        vertex_element = PlyElement.describe(array_data, 'vertex')
+        PlyData([vertex_element]).write(path)
 
     @staticmethod
     def load_ply(path: str, max_sh_order: int) -> dict:
-        """Load Gaussian model from PLY file"""
-        plydata = PlyData.read(path)
-
-        # Extract basic properties
-        mean = np.stack((
-            np.asarray(plydata.elements[0]["x"]),
-            np.asarray(plydata.elements[0]["y"]),
-            np.asarray(plydata.elements[0]["z"])
-        ), axis=1)
-        opacities = np.asarray(plydata.elements[0]["opacity"])[..., np.newaxis]
-
-        # Extract features
-        sh_coefficients_dc = np.zeros((mean.shape[0], 3, 1))
-        sh_coefficients_dc[:, 0, 0] = np.asarray(plydata.elements[0]["sh_coefficients_dc_0"])
-        sh_coefficients_dc[:, 1, 0] = np.asarray(plydata.elements[0]["sh_coefficients_dc_1"])
-        sh_coefficients_dc[:, 2, 0] = np.asarray(plydata.elements[0]["sh_coefficients_dc_2"])
-
-        # Extract rest features
-        extra_f_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("sh_coefficients_ac_")]
-        assert len(extra_f_names) == 3 * (max_sh_order + 1) ** 2 - 3
-        features_extra = np.zeros((mean.shape[0], len(extra_f_names)))
-        for idx, attr_name in enumerate(extra_f_names):
-            features_extra[:, idx] = np.asarray(plydata.elements[0][attr_name])
-        sh_coefficients_ac = features_extra.reshape((mean.shape[0], 3, (max_sh_order + 1) ** 2 - 1))
-
-        # Extract svec and quaternions
-        svec_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("svec_")]
-        svec = np.zeros((mean.shape[0], len(svec_names)))
-        for idx, attr_name in enumerate(svec_names):
-            svec[:, idx] = np.asarray(plydata.elements[0][attr_name])
-
-        rot_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("quaternion")]
-        rots = np.zeros((mean.shape[0], len(rot_names)))
-        for idx, attr_name in enumerate(rot_names):
-            rots[:, idx] = np.asarray(plydata.elements[0][attr_name])
-
+        ply_data = PlyData.read(path)
+        vertex_data = ply_data['vertex']
+        positions = np.column_stack((vertex_data['x'],vertex_data['y'],vertex_data['z']))
+        dc_channels = 3
+        dc_coeffs = np.zeros((positions.shape[0], dc_channels, 1))
+        dc_coeffs[:, 0, 0] = vertex_data['sh_coefficients_dc_0']
+        dc_coeffs[:, 1, 0] = vertex_data['sh_coefficients_dc_1']
+        dc_coeffs[:, 2, 0] = vertex_data['sh_coefficients_dc_2']
+        ac_props = [p.name for p in vertex_data.properties if p.name.startswith('sh_coefficients_ac_')]
+        ac_count = len(ac_props)
+        ac_coeffs = np.zeros((positions.shape[0], dc_channels, ac_count // dc_channels))
+        for i in range(ac_count):
+            chan = i % dc_channels
+            idx = i // dc_channels
+            ac_coeffs[:, chan, idx] = vertex_data[f'sh_coefficients_ac_{i}']
+        opacities = vertex_data['opacity'].reshape(-1, 1)
+        
+        scale_props = [p.name for p in vertex_data.properties if p.name.startswith('svec_')]
+        scales = np.column_stack([vertex_data[p] for p in scale_props])
+        rot_props = [p.name for p in vertex_data.properties 
+                    if p.name.startswith('quaternion_')]
+        rotations = np.column_stack([vertex_data[p] for p in rot_props])
         return {
-            "mean": mean,
-            "sh_coefficients_dc": sh_coefficients_dc,
-            "sh_coefficients_ac": sh_coefficients_ac,
-            "opacities": opacities,
-            "svec": svec,
-            "quaternions": rots
-        }
-    
-
+            "mean": positions,"sh_coefficients_dc": dc_coeffs,"sh_coefficients_ac": ac_coeffs,"opacities": opacities,"svec": scales,"quaternions": rotations}
